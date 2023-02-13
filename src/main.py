18b690dbdc13a2e419c4b411a42d2ad0bcbeb444
sampath <118274231+sampath017@users.noreply.github.com>
2023-02-13 10:38:59 +0530

    Chapter 4

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ def main():
     from nnfs.datasets import spiral_data
 
     from network.layers import Dense
+    from network.activations import ReLU, Softmax
 
     nnfs.init()
 
@@ -10,9 +11,16 @@ def main():
     X, y = spiral_data(100, 3)
 
     dense1 = Dense(2, 3)
+    activation1 = ReLU()
+    dense2 = Dense(3, 3)
+    activation2 = Softmax()
+
     dense1.forward(X)
-    
-    print(dense1.output[:5])
+    activation1.forward(dense1.output)
+    dense2.forward(activation1.output)
+    activation2.forward(dense2.output)
+
+    print(activation2.output[:5])
 
 
 if __name__ == "__main__":
