ed3c204663523b07bda557f6fca5bcb28dc3a645
sampath017 <118274231+sampath017@users.noreply.github.com>
2023-02-16 14:04:10 +0000

    Chapter 14

--- src/main.py
+++ src/main.py
@@ -9,11 +9,11 @@ from network.metrics import Accuracy
 
 nnfs.init()
 
-X, y = spiral_data(100, 3)
+X, y = spiral_data(9000, 3)
 
-dense1 = Dense(2, 64)
+dense1 = Dense(2, 512, l2_weight_regularizer=5e-4, l2_bias_regularizer=5e-4)
 activation1 = ReLU()
-dense2 = Dense(64, 3)
+dense2 = Dense(512, 3, l2_weight_regularizer=5e-4, l2_bias_regularizer=5e-4)
 activation_loss = ActivationSoftmaxLossCategoricalCrossentropy()
 optimizer = Adam(lr=0.05, lr_decay=5e-7)
 
@@ -24,13 +24,17 @@ for epoch in range(EPOCHS+1):
     dense1.forward(X)
     activation1.forward(dense1.output)
     dense2.forward(activation1.output)
-    loss = activation_loss.forward(dense2.output, y)
+
+    data_loss = activation_loss.forward(dense2.output, y)
+    regularization_loss = activation_loss.loss.regularization_loss(
+        dense1) + activation_loss.loss.regularization_loss(dense2)
+    loss = data_loss + regularization_loss
 
     # Metrics
     accuracy = Accuracy(activation_loss.output, y)
     if epoch % 100 == 0:
         print(
-            f"epoch: {epoch} acc: {accuracy:.3f} loss: {loss:.3f} lr: {optimizer.current_lr:.3f}")
+            f"epoch: {epoch} acc: {accuracy:.3f} (data_loss: {data_loss:.3f} regularization_loss: {regularization_loss:.3f}) loss: {loss:.3f}  lr: {optimizer.current_lr:.3f}")
 
     # Backpass
     activation_loss.backward(activation_loss.output, y)
@@ -45,7 +49,7 @@ for epoch in range(EPOCHS+1):
     optimizer.post_update_params()
 
 # Testing model
-X_test, y_test = spiral_data(100, 3)
+X_test, y_test = spiral_data(1000, 3)
 
 # Forward pass
 dense1.forward(X_test)
--- src/network/layers.py
+++ src/network/layers.py
@@ -2,9 +2,15 @@ import numpy as np
 
 
 class Dense:
-    def __init__(self, n_inputs, n_neurons):
+    def __init__(self, n_inputs, n_neurons,
+                 l1_weight_regularizer=0, l2_weight_regularizer=0,
+                 l1_bias_regularizer=0, l2_bias_regularizer=0):
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
+        self.l1_weight_regularizer = l1_weight_regularizer
+        self.l2_weight_regularizer = l2_weight_regularizer
+        self.l1_bias_regularizer = l1_bias_regularizer
+        self.l2_bias_regularizer = l2_bias_regularizer
 
     def forward(self, inputs):
         self.inputs = inputs
@@ -13,4 +19,23 @@ class Dense:
     def backward(self, dvalues):
         self.dweights = np.dot(self.inputs.T, dvalues)
         self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+
+        if self.l1_weight_regularizer > 0:
+            dL1 = np.ones_like(self.weights)
+            dL1[self.weights < 0] = -1
+            self.dweights += self.l1_weight_regularizer * dL1
+
+        if self.l1_bias_regularizer > 0:
+            dL1 = np.ones_like(self.biases)
+            dL1[self.biases < 0] = -1
+            self.dbiases += self.l1_bias_regularizer * dL1
+
+        if self.l2_weight_regularizer > 0:
+            self.dweights += 2 * self.l2_weight_regularizer * \
+                self.weights
+
+        if self.l2_bias_regularizer > 0:
+            self.dbiases += 2 * self.l2_bias_regularizer * \
+                self.biases
+
         self.dinputs = np.dot(dvalues, self.weights.T)
